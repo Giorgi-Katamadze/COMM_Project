7d426560cbc5f75d12474ee371452ddacf1d0154
J2K4UNM <giorgikatamadze718@gmail.com>
2023-04-15 21:45:01 +0400

    2

---
+++ Todo.md
@@ -0,0 +1,30 @@
+# Ecommerce
+
+## First page
+1. Find Api and website design
+2. Start design for phone resolution
+a. NAV
+Logo, 
+BurgerMenu, 
+ShopCart
+b. BurgerMenu
+HOme, About, Contact, LogIn, SignUp
+c. ShopCart
+All the items choosed and button for payment
+(for payment another page with form for card numbers and submit)
+d. LogIn insame page
+3. Header video with text animation
+4. Features
+5. All items with animation display, first clothing, then jewelery, then tecnic and all items in another page
+6. Contact and Footer together
+
+## All items page
+1. Same nav
+2. h1 "all items"
+3. innerHTML all items from data
+
+## SigUp page
+1. Left side photo Right side form with submit button
+
+## Payment page
+1. just a form for card numbers and submit button
--- src/script/script.js
+++ src/script/script.js
@@ -1,18 +1,67 @@
-const API = "https://fakestoreapi.com/products"
-
-getData(API)
-async function getData(url){
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-    displayProducts(data);
+const API = "https://fakestoreapi.com/products";
+getData(API);
+async function getData(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+  displayClothes(data)
+  displayJewelry(data)
+  displaytechnic(data)
 }
 
-const divHTML = document.getElementById("data")
-function displayProducts(data){
-    data.forEach((product) => {
-        divHTML.innerHTML += `
-        <img src="${product.image}">
-        `
+const textEl = document.getElementById("videoText")
+let index = 0
+const text = `Search
+Find
+And Purchase`
+let speed = 100
+function writeText() {
+    textEl.innerText = text.slice(0,index)
+    if(index < text.length){
+        index++
+        setTimeout(writeText, speed)
+    }
+ }
+writeText()
+
+function displayClothes(data){
+    const clothesData = data.filter(item => item.category === "men's clothing" || item.category === "women's clothing")
+    console.log(clothesData);
+    const container = document.getElementById('clothes');
+    clothesData.forEach(item => {
+        container.innerHTML += `
+        <div>
+            <img src="${item.image}" id="itemImg" class="containerImg">
+            <p>${item.price}$</p>
+        </div>
+        `;
     });
 }
+function displayJewelry(data){
+    const jewelryData = data.filter(item => item.category === "jewelery")
+    console.log(jewelryData);
+    const container = document.getElementById('jewelry');
+    jewelryData.forEach(item => {
+        container.innerHTML += `
+        <div>
+            <img src="${item.image}" id="itemImg" class="containerImg">
+            <p>${item.price}$</p>
+        </div>
+        `;
+    });
+}
+function displaytechnic(data){
+    const technicData = data.filter(item => item.category === "electronics")
+    console.log(technicData);
+    const container = document.getElementById('technic');
+    technicData.forEach(item => {
+        container.innerHTML += `
+        <div>
+            <img src="${item.image}" id="itemImg" class="containerImg">
+            <p>${item.price}$</p>
+        </div>
+        `;
+    });
+}
+
+
